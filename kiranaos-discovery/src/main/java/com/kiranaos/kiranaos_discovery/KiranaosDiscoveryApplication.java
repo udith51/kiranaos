a6e1f881a2f2bcd5197dc98e0d0402154f0adbe1
Udith Shyamsukha <udith@m0.ventures>
2026-03-26 12:06:31 +0530

    chore: scaffold monorepo with discovery stub

---
+++ kiranaos-discovery/src/main/java/com/kiranaos/kiranaos_discovery/KiranaosDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.kiranaos.kiranaos_discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiranaosDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiranaosDiscoveryApplication.class, args);
+	}
+
+}
---
+++ kiranaos-discovery/src/test/java/com/kiranaos/kiranaos_discovery/KiranaosDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiranaos.kiranaos_discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiranaosDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
