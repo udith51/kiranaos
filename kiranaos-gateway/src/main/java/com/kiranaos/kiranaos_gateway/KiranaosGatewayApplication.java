c9b50f335b205edf4096d59b99a187450d5badfd
Udith Shyamsukha <udith@m0.ventures>
2026-03-26 14:26:27 +0530

    feat(gateway): add spring cloud gateway with eureka routing

---
+++ kiranaos-gateway/src/main/java/com/kiranaos/kiranaos_gateway/KiranaosGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.kiranaos.kiranaos_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiranaosGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiranaosGatewayApplication.class, args);
+	}
+
+}
---
+++ kiranaos-gateway/src/test/java/com/kiranaos/kiranaos_gateway/KiranaosGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiranaos.kiranaos_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiranaosGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
