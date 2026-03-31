00f057b45c6658bcc0ce077dd0f5501b2a854f45
Udith Shyamsukha <udith@m0.ventures>
2026-03-31 23:00:09 +0530

    added workflows for ci

--- kiranaos-gateway/src/test/java/com/kiranaos/kiranaos_gateway/KiranaosGatewayApplicationTests.java
+++ kiranaos-gateway/src/test/java/com/kiranaos/kiranaos_gateway/KiranaosGatewayApplicationTests.java
@@ -2,12 +2,22 @@ package com.kiranaos.kiranaos_gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class KiranaosGatewayApplicationTests {
+
+	@LocalServerPort
+	private int port;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void gatewayPortShouldBe8080(){
+		assertEquals(8080, port);
+	}
 }
