a6dbfac477011e4d639fef7b0b1666af3da96082
Udith Shyamsukha <udith@m0.ventures>
2026-04-06 17:17:43 +0530

    setup store service

---
+++ kiranaos-store-service/src/main/java/com/kiranaos/kiranaos_store_service/KiranaosStoreServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kiranaos.kiranaos_store_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiranaosStoreServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiranaosStoreServiceApplication.class, args);
+	}
+
+}
