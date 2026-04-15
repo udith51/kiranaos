11b05b8d6ffa8ff9bc9c6612fe1c705c6fe6677b
Udith Shyamsukha <udith@m0.ventures>
2026-04-16 00:56:21 +0530

    scaffold billing service

---
+++ kiranaos-billing-service/src/main/java/com/kiranaos/kiranaos_billing_service/KiranaosBillingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kiranaos.kiranaos_billing_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiranaosBillingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KiranaosBillingServiceApplication.class, args);
+    }
+
+}
